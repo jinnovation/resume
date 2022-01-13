98a3f694eb6f7c07e331e3fbb6bd4f737885655a
Jonathan Jin <me@jonathanj.in>
2022-01-12 23:53:00 -0500

    Add rudimentary nix-shell config

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  # need to install source-sans-pro separately...
+  name = "jjin-resume-env";
+  buildInputs = with pkgs; [
+    source-sans
+    (texlive.combine {
+      inherit (texlive) scheme-small
+        enumitem
+        environ
+        fontawesome5
+        fontawesome
+        fontspec
+        luainputenc
+        preprint
+        tcolorbox
+        titlesec;
+    })
+  ];
+}
