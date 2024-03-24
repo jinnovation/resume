594aee8296df39e9af77cc0540f023cc430239c2
Jonathan Jin <me@jonathanj.in>
2024-03-24 08:20:09 -0400

    dir-locals: add compilation cmd

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,2 @@
-((typst-ts-mode . ((eval . (add-hook 'before-save-hook #'delete-trailing-whitespace t t)))))
+((nil . ((projectile-project-compilation-cmd . "typst compile resume.typ")))
+ (typst-ts-mode . ((eval . (add-hook 'before-save-hook #'delete-trailing-whitespace t t)))))
